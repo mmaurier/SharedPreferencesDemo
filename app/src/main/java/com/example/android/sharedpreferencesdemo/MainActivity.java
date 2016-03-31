c658231202510fd0d4e1d52eb797d922fd701fe0
mmaurier <marco.maurier@mail.wvu.edu>
2016-03-31 09:35:45 -0400

    Initial commit edit menu button

---
+++ app/src/main/java/com/example/android/sharedpreferencesdemo/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.android.sharedpreferencesdemo;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.android.sharedpreferencesdemo", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("username", "marco").apply();
+        String username = sharedPreferences.getString("username","");
+        Log.i("username", username);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add) {
+            Log.i("action button tapped","add");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
